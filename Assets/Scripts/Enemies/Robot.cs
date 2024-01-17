27066c7ae84058a555e69fa86d7ff27764a7df07
victorliu1231 <43052540+victorliu1231@users.noreply.github.com>
2024-01-17 00:40:16 -0500

    Uploading Corgi Engine

--- Assets/Scripts/Enemies/Robot.cs
+++ Assets/Scripts/Enemies/Robot.cs
@@ -8,6 +8,7 @@ public class Robot : EnemyWithPathfinding
     public Vector2 force;
     public float damageAmount;
 
+    #pragma warning disable 0114
     // Start is called before the first frame update
     void Start()
     {
@@ -26,6 +27,7 @@ public class Robot : EnemyWithPathfinding
         wallLayer = LayerMask.GetMask("Wall");
         boxColliderHeight = GetComponent<BoxCollider2D>().size.y * transform.localScale.y; // Need to multiply by y-scale to get correct scaling relationship
     }
+    #pragma warning restore 0114
 
     // When robot collides with player, throw player in the air.
     void OnCollisionEnter2D(Collision2D collision){
