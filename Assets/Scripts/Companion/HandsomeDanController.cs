10d1ee2822f6412c4b716e0b63dafd2f87417b0b
aryaan02 <aryaan.khan@yale.edu>
2024-02-17 14:39:32 -0500

    Add HandsomeDan item collection mechanic

---
+++ Assets/Scripts/Companion/HandsomeDanController.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class HandsomeDanController : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public float targetDetectionRange = 5.0f;
+    private GameObject target = null;
+    public int points = 30;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        if (target == null){
+            FindNearestTarget();
+
+            if (target == null){
+                FollowPlayer();
+            }
+        }
+        else{
+            MoveTowardsTarget();
+        }
+    }
+
+    void FindNearestTarget(){
+        List<GameObject> targets = new List<GameObject>();
+
+        GameObject[] coins = GameObject.FindGameObjectsWithTag("Coin");
+        GameObject[] assignments = GameObject.FindGameObjectsWithTag("Assignment");
+
+        targets.AddRange(coins);
+        targets.AddRange(assignments);
+        float nearestDistance = Mathf.Infinity;
+        GameObject nearestTarget = null;
+        
+        foreach (GameObject potentialTarget in targets){
+            float distance = Vector3.Distance(potentialTarget.transform.position, transform.position);
+            if (distance < nearestDistance){
+                nearestDistance = distance;
+                nearestTarget = potentialTarget;
+            }
+        }
+        
+        Transform playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        float distanceToPlayer = Vector3.Distance(playerTransform.position, nearestTarget.transform.position);
+        if (distanceToPlayer < targetDetectionRange){
+            target = nearestTarget;
+        }
+    }
+
+    void MoveTowardsTarget(){
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.transform.position, step);
+
+        if (Vector3.Distance(transform.position, target.transform.position) < 0.001f){
+            // Get player inventory
+            PlayerInventory playerInventory = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerInventory>();
+            if (playerInventory != null)
+            {
+                playerInventory.ItemCollected(target); // Call the ItemCollected method in the PlayerInventory script.
+                target.SetActive(false);
+                ScoreManager.Instance.AddPoints(points);
+                target = null;
+            }
+        }
+    }
+
+    void FollowPlayer(){
+        float step = speed * Time.deltaTime;
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        Vector3 newPosition = new Vector3(player.transform.position.x - 2, player.transform.position.y, player.transform.position.z);
+        transform.position = Vector3.MoveTowards(transform.position, newPosition, step);
+    }
+}
